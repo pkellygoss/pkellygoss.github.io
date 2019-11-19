3b6ca830ec6835987479a0192d364a52d04f7329
pkellygoss <43387854+pkellygoss@users.noreply.github.com>
2019-11-19 09:17:15 +0000

    added embeds

---
+++ EmbedIframe/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmbedIframe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
